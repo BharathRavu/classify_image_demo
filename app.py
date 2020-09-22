fb4e264c301198d3be4f22510e54c75d0e2e88a2
BharathRavu <61087372+BharathRavu@users.noreply.github.com>
2020-09-23 01:53:54 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 23 00:47:25 2020
+
+@author: bharath
+"""
+
+from __future__ import division, print_function
+# coding=utf-8
+import os
+import numpy as np
+
+# Keras
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+
+# Flask utils
+from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
+#from gevent.pywsgi import WSGIServer
+
+# Define a flask app
+app = Flask(__name__)
+
+# Model saved with Keras model.save()
+MODEL_PATH ='mobilenetv2_model.h5'
+
+# Load your trained model
+model = load_model(MODEL_PATH)
+
+def model_predict(img_path, model):
+    print(img_path)
+    img = image.load_img(img_path, target_size=(224, 224))
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    # x = np.true_divide(x, 255)
+    ## Scaling
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+   
+
+    # Be careful how your trained model deals with the input
+    # otherwise, it won't make correct prediction!
+   # x = preprocess_input(x)
+
+    preds = model.predict(x)
+    preds=np.argmax(preds, axis=1)
+    if preds==0:
+        preds="Cobra"
+    elif preds==1:
+        preds="Neam Leaves"
+    else:
+        preds="Sadhguru"
+    return preds
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model)
+        result=preds
+        return result
+    return None
+
+
+if __name__ == '__main__':
+    app.run(port=5001,debug=True)
